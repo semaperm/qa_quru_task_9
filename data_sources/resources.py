cf2c4523dc56385fc86f767a8e8e83efd204ed6c
Sema Butsyk <semakontakt@mail.ru>
2025-03-20 17:11:09 +0500

    ver 8

--- data_sources/resources.py
+++ data_sources/resources.py
@@ -5,5 +5,5 @@ import tests
 
 def path(file_name):
     return str(
-        Path(tests.__file__).parent.joinpath(f'test/{file_name}').absolute()
+        Path(tests.__file__).parent.joinpath(f'file/{file_name}').absolute()
     )

723ef90170a58527f3e3d0df2a1fdffc9ac0d197
Sema Butsyk <semakontakt@mail.ru>
2025-03-20 14:42:51 +0500

    ver 6

--- demoqa_tests/practice_form_page.py
+++ demoqa_tests/practice_form_page.py
@@ -1,60 +1,73 @@
-import os
-from selene import browser, have, be, by
+from selene import browser, by, have
+from data_sources import resources
 
-class RegistrationPage:
+
+class PracticeFormPage:
+    def __init__(self):
+        self.first_name = browser.element('[id="firstName"]')
+        self.last_name = browser.element('[id="lastName"]')
+        self.email = browser.element('[id="userEmail"]')
+        self.user_number = browser.element('[id="userNumber"]')
+        self.address = browser.element('[id="currentAddress"]')
+        self.subject = browser.element("#subjectsInput")
 
     def open(self):
         browser.open('/automation-practice-form')
         browser.driver.execute_script("$('#fixedban').remove()")
         browser.driver.execute_script("$('footer').remove()")
 
-    def fill_full_name(self, first_name, last_name):
-        browser.element("#firstName").should(be.blank).type(first_name)
-        browser.element("#lastName").should(be.blank).type(last_name)
+    def fill_first_name(self, value):
+        self.first_name.type(value)
+
+    def fill_last_name(self, value):
+        self.last_name.type(value)
 
     def fill_email(self, value):
-        browser.element("#userEmail").should(be.blank).type(value)
+        self.email.type(value)
 
-    def fill_gender(self, value):
+    def select_gender(self, value):
         browser.element(by.text(value)).click()
 
     def fill_user_number(self, value):
-        browser.element("#userNumber").should(be.blank).type(value).press_tab()
+        self.user_number.type(value)
 
+    def pick_date_of_birth(self, year, month, day):
+        browser.element("#dateOfBirthInput").click()
+        browser.element("[class='react-datepicker__year-select']").click().element(by.text(f"{year}")).click()
+        browser.element("[class='react-datepicker__month-select']").click().element(by.text(f"{month}")).click()
+        browser.element(by.text(f"{day}")).click()
 
-    def fill_date_of_birth(self, day, month, year):
-        browser.element('#dateOfBirth').click().element(f'[value="{month - 1}"]').click()
-        browser.element('#dateOfBirth').element(f'[value="{year}"]').click()
-        browser.element('.react-datepicker__month').element(by.text(f'{day}')).click()
+    def fill_subject(self, value):
+        self.subject.type(value).press_enter()
 
-    def fill_subject(self, subject):
-        browser.element('#subjectsInput').click().type(subject).should(be.visible).press_enter()
-
-    def fill_interest_sport(self):
+    def choose_interest_sport(self):
         browser.element(by.text("Sports")).click()
 
-    def fill_interest_music(self):
-        browser.element(by.text("Reading")).click()
-
-    def fill_interest_reading(self):
+    def choose_interest_music(self):
         browser.element(by.text("Music")).click()
 
-    def fill_picture(self, value):
-        browser.element('[id="uploadPicture"]').set_value(os.path.abspath(value))
+    def choose_interest_reading(self):
+        browser.element(by.text("Reading")).click()
 
-    def fill_current_address(self, current_address):
-        browser.element('#currentAddress').should(be.blank).type(current_address)
+    def upload_picture(self, value):
+        browser.element('#uploadPicture').set_value(resources.path(value))
 
-    def fill_state_and_city(self, state, city):
-        browser.element('#state').click()
-        browser.element('#react-select-3-input').type(state).should(be.visible).press_enter()
-        browser.element('#city').click()
-        browser.element('#react-select-4-input').type(city).should(be.visible).press_enter()
+    def choose_state(self, value):
+        browser.element('[id="state"]').click()
+        browser.element(by.text(value)).click()
+
+    def choose_city(self, value):
+        browser.element('[id="city"]').click()
+        browser.element(by.text(value)).click()
+
+    def fill_address(self, value):
+        self.address.type(value)
 
     def submit_button(self):
-        browser.element("#submit").click()
+        browser.element('[id="submit"]').click()
 
     def should_registered_user_with(self, full_name, email, gender, user_number, birthdate, subjects, hobby, file, address, location):
+
         browser.element('.table').all('td').even.should(
             have.exact_texts(
                 full_name,
---
+++ tests/tests_for_practice_form.py
@@ -0,0 +1,34 @@
+from demoqa_tests.practice_form_page import PracticeFormPage
+
+
+def test_practice_form():
+    practice_form_page = PracticeFormPage()
+    practice_form_page.open()
+    practice_form_page.fill_first_name("Sema")
+    practice_form_page.fill_last_name("Butsyk")
+    practice_form_page.fill_email("test@test.ru")
+    practice_form_page.select_gender("Male")
+    practice_form_page.fill_user_number("1231231234")
+    practice_form_page.pick_date_of_birth("1991", "September", "20")
+    practice_form_page.fill_subject("Maths")
+    practice_form_page.choose_interest_sport()
+    practice_form_page.choose_interest_music()
+    practice_form_page.choose_interest_reading()
+    practice_form_page.upload_picture("Proverka.jpg")
+    practice_form_page.fill_address("City Name, Street Name")
+    practice_form_page.choose_state("NCR")
+    practice_form_page.choose_city("Noida")
+    practice_form_page.submit_button()
+
+    practice_form_page.should_registered_user_with(
+        "Sema Butsyk",
+        "test@test.ru",
+        "Male",
+        "1231231234",
+        "20 September,1995",
+        "Maths",
+        "Sports, Music, Reading",
+        "image.jpg",
+        "City Name, Street Name",
+        "NCR Noida"
+    )
